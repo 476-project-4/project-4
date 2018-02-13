e960688741d6873b31eef980cb3ed8465dcbac41
daniel <danielsollis2@gmail.com>
2018-02-12 19:21:26 -0800

    first commit

---
+++ minitwit/population.sql
@@ -0,0 +1,23 @@
+INSERT INTO follower (who_id, whom_id)
+VALUES (1, 3);
+
+INSERT INTO follower (who_id, whom_id)
+VALUES (3, 1);
+
+INSERT INTO follower (who_id, whom_id)
+VALUES (2, 4);
+
+INSERT INTO follower (who_id, whom_id)
+VALUES (4, 1);
+
+Insert Into message (author_id, text, pub_date)
+VALUES (1, 'yo', 201802090607);
+
+Insert Into message (author_id, text, pub_date)
+VALUES (2, 'hey', 201802090607);
+
+Insert Into message (author_id, text, pub_date)
+VALUES (3, 'sup', 201802090607);
+
+Insert Into message (author_id, text, pub_date)
+VALUES (4, '!', 201802090607);
